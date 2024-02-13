ac6757245c5afb8273716facd8726437271f0289
hunkyburrito <hunkyburrito@protonmail.com>
2024-02-13 16:56:32 -0600

    Added my options

--- lua/options.lua
+++ lua/options.lua
@@ -6,10 +6,11 @@
 vim.o.hlsearch = false
 
 -- Make line numbers default
-vim.wo.number = true
+--vim.wo.number = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+--vim.o.mouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -40,3 +41,26 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 -- vim: ts=2 sts=2 sw=2 et
+
+-- BURRITO OPTS
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undodir = vim.fn.expand('~/.config/nvim/undo//')
+vim.o.backupdir = vim.fn.expand('~/.config/nvim/backup//')
+vim.o.directory = vim.fn.expand('~/.config/nvim/swap//')
+vim.o.guifont = 'RobotoMono Nerd Font Mono'
+vim.o.diffopt = 'filler,context:3,vertical,closeoff'
+--vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.scrolloff = 8
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
