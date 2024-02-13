d16daa8ba2107f1299d08f4c88bae1fcec7051a4
hunkyburrito <hunkyburrito@protonmail.com>
2024-02-13 16:56:16 -0600

    Added my keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,3 +26,50 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- vim: ts=2 sts=2 sw=2 et
+
+-- BURRITO BINDS
+-- Mappings
+vim.keymap.set('i', '<C-l>', '<Left>')
+vim.keymap.set('i', '<C-r>', '<Right>')
+vim.keymap.set('i', '<C-d>', '<Down>')
+vim.keymap.set('i', '<C-u>', '<Up>')
+vim.keymap.set('i', 'jk', '<ESC>')
+vim.keymap.set('n', '<leader>o', 'o<ESC>')
+vim.keymap.set('n', '<leader>O', 'O<ESC>')
+vim.keymap.set('n', '<leader>h', ':set invhlsearch<CR>')
+
+-- Center Cursor
+vim.keymap.set({'n', 'v'}, '<C-d>', '<C-d>zz')
+vim.keymap.set({'n', 'v'}, '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-j>', 'mzi<CR><ESC>`z')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Visual Moving
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Preserve yank after delete
+vim.keymap.set('x', '<leader>p', '"_dP')
+
+-- Yank to Clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+
+-- Paste to Clipboard
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
+
+-- Preserve Registers after delete
+vim.keymap.set({'n', 'v'}, '<leader>d', '"_d')
+vim.keymap.set('n', '<leader>c', '"_c')
+
+-- Format Code
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+
+-- Quick Fix
+vim.keymap.set('n', '<leader>k', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<C-M-n>', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<C-M-p>', '<cmd>lprev<CR>zz')
