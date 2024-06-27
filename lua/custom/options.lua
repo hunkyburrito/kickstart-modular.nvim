cf246696d1adfd154672f7db58c2302bbc70438d
hunkyburrito <hunkyburrito@protonmail.com>
2024-06-26 20:04:03 -0500

    Update custom/options

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,18 +1,24 @@
 -- BURRITO OPTS
-vim.o.swapfile = false
+--vim.o.autoread = true
+vim.o.autochdir = true
+vim.o.undofile = true
 vim.o.backup = false
+vim.o.swapfile = false
 vim.o.undodir = vim.fn.expand '~/.config/nvim/undo//'
 vim.o.backupdir = vim.fn.expand '~/.config/nvim/backup//'
 vim.o.directory = vim.fn.expand '~/.config/nvim/swap//'
-vim.o.guifont = 'RobotoMono Nerd Font Mono'
+vim.o.guifont = 'MonaspiceNe Nerd Font Mono'
 vim.o.diffopt = 'filler,context:3,vertical,closeoff'
 --vim.o.wrap = false
-vim.o.tabstop = 4
+vim.o.tabstop = 8
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
-vim.o.expandtab = true
+vim.o.expandtab = false
 vim.o.smartindent = true
+vim.o.smarttab = true
 vim.o.scrolloff = 8
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
