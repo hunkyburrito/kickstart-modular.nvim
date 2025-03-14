ac46907e29e738627dbe5f88578ccc5bf722016a
hunkyburrito <hunkyburrito@protonmail.com>
2025-03-14 17:31:40 -0500

    remove cpplint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         --c = { 'cpplint' },
-        cpp = { 'cpplint' },
+        --cpp = { 'cpplint' },
         --python = { 'ruff' },
         bash = { 'shellcheck' },
         html = { 'htmlhint' },
