1dd0da522ab447a21932ae539523a45f003bdd23
hunkyburrito <hunkyburrito@protonmail.com>
2025-10-15 10:57:12 -0500

    folds

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,6 +1,6 @@
 -- BURRITO OPTS
 --vim.o.autoread = true
---vim.o.autochdir = true
+vim.o.autochdir = true
 vim.o.undofile = true
 vim.o.backup = false
 vim.o.swapfile = true
@@ -13,16 +13,23 @@ vim.o.diffopt = 'filler,context:3,vertical,closeoff'
 vim.o.tabstop = 8
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
-vim.o.expandtab = false
+vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.smarttab = true
 vim.o.scrolloff = 8
 vim.o.splitright = true
 vim.o.splitbelow = true
 vim.opt.fillchars:append { diff = '/' }
+vim.opt.foldcolumn = '0'
+--vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 
 -- cursor goes one right after exiting insert
-vim.api.nvim_create_autocmd("InsertLeave", { pattern = "*", command = ":normal! `^"})
+vim.api.nvim_create_autocmd('InsertLeave', { pattern = '*', command = ':normal! `^' })
 vim.o.virtualedit = 'onemore'
 
 -- You can configure highlights by doing something like:
@@ -33,13 +40,14 @@ vim.cmd.hi 'DiffChange guibg=#203850'
 vim.cmd.hi 'DiffText guibg=#205895'
 
 -- turn off lsp diagnostics for diffs
-local function check_diff() return not vim.wo.diff end
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+local function check_diff()
+  return not vim.wo.diff
+end
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   underline = check_diff,
   signs = check_diff,
   update_in_insert = check_diff,
-  virtual_text = check_diff
+  virtual_text = check_diff,
 })
 
 vim.g.loaded_netrw = 1
